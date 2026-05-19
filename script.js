a76b476dab3bbe21a1cde4c78b993773c4925801
Kaike Corrêa Panetto <kaikepanetto@gmail.com>
2026-05-19 12:32:47 -0300

    melhores os cards e adicionei JS

---
+++ script.js
@@ -0,0 +1,9 @@
+function crescer1() {
+    document.getElementById('res1').classList.toggle('aberto');
+}
+function crescer2() {
+    document.getElementById('res2').classList.toggle('aberto');
+}
+function crescer3() {
+    document.getElementById('res3').classList.toggle('aberto');
+}
