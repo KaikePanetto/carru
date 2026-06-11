26d43cc843e6cdb0c057616594673b7453668f9a
Kaike Corrêa Panetto <kaikepanetto@gmail.com>
2026-06-11 01:25:25 -0300

    modo escuro adicionado

--- script.js
+++ script.js
@@ -7,3 +7,21 @@ function crescer2() {
 function crescer3() {
     document.getElementById('res3').classList.toggle('aberto');
 }
+
+let escuro = false;
+
+function modoEscuro() {
+    escuro = !escuro;
+    document.querySelectorAll(".bg-aliceblue").forEach(secao => {
+        secao.style.backgroundColor = escuro ? "#888888" : "aliceblue";
+        secao.style.color = escuro ? "white" : "black";
+    });
+
+    document.querySelectorAll(".card").forEach(card => {
+        card.style.backgroundColor = escuro ? "#6e6e6e" : "white";
+
+        card.querySelectorAll("h5, h6, p").forEach(texto => {
+            texto.style.color = escuro ? "white" : "black";
+        });
+    });
+}
